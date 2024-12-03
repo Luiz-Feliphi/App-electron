260479792f19446ec72ab2e620666e4c0cd55d88
Luiz Feliphi <luiz.feliphi@estudante.ifgoiano.edu.br>
2024-12-03 00:03:57 -0300

    update0.8(urgente)

--- app/js/scriptCentral.js
+++ app/js/scriptCentral.js
@@ -1,6 +1,6 @@
 // Seleciona todos os links de navegação
 document.querySelectorAll('a').forEach(link => {
-    link.addEventListener('click', function(event) {
+    link.addEventListener('click', function() {
         // Remove a classe 'selecionado' de todos os itens
         document.querySelectorAll('.opcoes-lista').forEach(item => {
             item.classList.remove('selecionado');
@@ -15,17 +15,15 @@ document.querySelectorAll('a').forEach(link => {
         // Atualiza o valor do elemento com o ID 'titulo-fora' dependendo do item clicado
         const tituloFora = document.getElementById('titulo-fora');
         const sobre = document.getElementById('sobre');
-        const america_central = document.getElementById('opcao1');
-        const america_sul = document.getElementById('opcao2');
         const lateral = document.querySelector('#image-lateral');
         const imagem = document.getElementById('image-sobre');
+        const tituloAmericaC = document.getElementById('titulo-fora2'); 
         if(id == 'countries'){
-            imagem.style.display = 'none';
-            sobre.textContent = '';
+            imagem.style.opacity = '0';
+        } else{
+            imagem.style.opacity = '1'
         }
         if(id != 'countries'){
-            america_central.style.display = "none";
-            america_sul.style.display = "none";
             lateral.style.opacity = "1";
         }
         switch (id) {
@@ -33,19 +31,18 @@ document.querySelectorAll('a').forEach(link => {
                 tituloFora.textContent = 'Paises do continente americano';
                 break;
                 
-                case 'browser':
-                    tituloFora.textContent = 'Encontre algum pais do continente latino americano';
-                    break;
+            case 'browser':
+                tituloFora.textContent = 'Encontre algum pais do continente latino americano';
+            break;
                     
-                    case 'countries':
-                        tituloFora.textContent = 'Que parte da america você deseja ver sobre?';
-                        tituloFora.style.fontSize = "x-large";
-                america_central.style.display = "block";
-                america_sul.style.display = "block";
-                break;
-            }
+            case 'countries':
+                tituloFora.style.display = 'none';
+                tituloAmericaC.style.display = 'block';
+                tituloFora.style.fontSize = "x-large";
+            break;
+        }
             //texto sobre
-            switch (id) {
+        switch (id) {
             case 'home':
                 sobre.textContent = 'Que tal ver mais sobre eles?!';
             break;
